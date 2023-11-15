e052ceb899bf4c69c54d9efce633254f4bc9485f
HoangShiro <hoangdao13999@gmail.com>
2023-11-16 03:42:13 +0700

    Update

--- utils/bot.py
+++ utils/bot.py
@@ -12,25 +12,39 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 CAc = PyAsyncCAI(cai_key)
 CAcr = c_token
 
+ai_name = "Himeka"
+ai_last = "Shindou"
+ai_full_name = f"{ai_name} {ai_last}"
+
 @bot.event
 async def on_ready():
+    # Đồng bộ hoá commands
+    try:
+        synced = await bot.tree.sync()
+        print(f"Đã đồng bộ {len(synced)} lệnh.")
+    except Exception as e:
+        print(e)
     print("Himeka đã khởi động")
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-    user_name = message.author.nick
-    if not user_name:
-        user_name = message.author.name
-    result = "{}: {}".format(user_name, message.content)
-    print(result)
+    
     # Phản hồi chat
-    async with message.channel.typing():
-        answ, ain = await CAI(result)
-        print(f"{ain}: {answ}")
-        print()
-        await message.reply(answ)
+    if message.content:
+        user_name = message.author.nick
+        if not user_name:
+            user_name = message.author.name
+        umess = "{}: {}".format(user_name, message.content)
+        print(umess)
+        async with message.channel.typing():
+            answ, ain = await CAI(umess)
+            print(f"{ain}: {answ}")
+            print()
+            await message.reply(answ)
+    
+    return
 
 # Hàm Chat
 async def CAI(message):
@@ -52,6 +66,16 @@ async def CAI(message):
     text = data['replies'][0]['text']
     
     return text, name
+
+# Các câu lệnh
+@bot.tree.command(name="renew", description=f"Khởi động lại {ai_name}.")
+async def renew(interaction: discord.Interaction):
+    await interaction.response.send_message(f"{ai_name} sẽ sớm quay lại nè~!", ephemeral=True)
+    await bot.close()
+
+@bot.tree.command(name="newchat", description="Cuộc trò chuyện mới.")
+async def newchat(interaction: discord.Interaction):
+    CAc.chat.new_chat(c_token)
 
 def bot_run():
     bot.run(discord_bot_key)
