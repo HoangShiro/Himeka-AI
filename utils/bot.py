92913262723ccad9755e89b53d7bed2fc430d58a
HoangShiro <hoangdao13999@gmail.com>
2023-11-18 23:21:32 +0700

    Test

--- utils/bot.py
+++ utils/bot.py
@@ -386,7 +386,7 @@ async def status_make():
         return emood
     
     emood = await set_emood(ai_status.bot_mood)
-    embed=discord.Embed(title="The Head of Libra's city", description="IW's card lv: 4")
+    embed=discord.Embed(title="The Head of Libra's city", description="IW's card lv: 4", color=0xffa3af)
     embed.set_author(name=f"{ai_full_name}", url="https://beta.character.ai/chat2?char=g9qGgwr7kJRARbsOV52ChcKaEkJYPUF1A3mprJmgUjs",
                      icon_url="https://safebooru.org//images/4420/b044860fbd8ee619f9d7e637010104ad.png")
     embed.set_thumbnail(url="https://safebooru.org//images/4420/b044860fbd8ee619f9d7e637010104ad.png")
