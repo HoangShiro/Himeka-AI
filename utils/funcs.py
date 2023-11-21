4f021245ffcdd72253ffb58b33db820d72b3b2f4
HoangShiro <hoangdao13999@gmail.com>
2023-11-22 02:54:06 +0700

    Test

--- utils/funcs.py
+++ utils/funcs.py
@@ -175,12 +175,12 @@ async def mess_rep(message, mess, user_name, chat_log):
             await message.reply(embed=embed, view=view)
         else: 
             await message.reply(answ)
-            asyncio.create_task(stt_inchat(user_name))
+            #asyncio.create_task(stt_inchat(user_name))
             #uid = message.author.id
             #u = UserData(uid)
             #u.update('u_fame', 1)
         #ai_status.update('total_chat', 1)
-        asyncio.create_task(hime_tablet(message, answ, chat_log, user_name))
+        #asyncio.create_task(hime_tablet(message, answ, chat_log, user_name))
         #await img_gen_chat(message, mess)
 
 # Send message
