a24ea9b46d84eb92bb016304743e815179a9fa7d
HoangShiro <hoangdao13999@gmail.com>
2023-11-24 21:40:57 +0700

    fix

--- utils/funcs.py
+++ utils/funcs.py
@@ -122,7 +122,7 @@ def remove_act(text):
     text = re.sub(r'\([^)]+\)', '', text)
     text = re.sub(r'<[^>]+>', '', text)
     text = re.sub(r'https?://\S+', '', text)
-    text = re.sub(r':3', '', text)
+    text = text.replace(':3', '')
     return text
 
 # Emoji load
