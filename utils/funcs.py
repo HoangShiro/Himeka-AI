19a1e1ab2ca3d3994f3a309213200b579d6517dd
HoangShiro <hoangdao13999@gmail.com>
2023-11-24 15:26:53 +0700

    Fix

--- utils/funcs.py
+++ utils/funcs.py
@@ -454,14 +454,13 @@ async def money_with_hime():
 
 # Keys update
 async def change_keys(file_path, key_name, new_key_value):
-
     with open(file_path, 'r') as config_file:
         content = config_file.read()
 
         pattern = re.compile(rf'{key_name} = .*')
         if re.search(pattern, content):
-            if isinstance(new_key_value, int):
-                # Nếu new_key_value là kiểu int, không thêm ngoặc kép
+            if str(new_key_value).isdigit():
+                # Nếu new_key_value chỉ chứa số, không thêm ngoặc kép
                 content = re.sub(pattern, f'{key_name} = {new_key_value}', content)
             else:
                 # Ngược lại, thêm ngoặc kép cho new_key_value
