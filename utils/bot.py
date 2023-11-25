dfe1e45452f5cdcf368b1131389c8d3174ec072a
HoangShiro <hoangdao13999@gmail.com>
2023-11-25 20:51:08 +0700

    test

--- utils/bot.py
+++ utils/bot.py
@@ -734,7 +734,7 @@ async def user_item(interaction: discord.Interaction, command: discord.Option(
     u = UserData(uid)
     noti = None
     if index:
-        index = int(index) + 1
+        index = int(index) - 1
     print(index)
     if "add" in command:
         noti = await u.add_item(iid, quantity, consum)
